a98a1cc90a86af2bc1c9788daef806c86cda4a27
jungryuel <zldrktduf0927@naver.com>
2024-06-17 17:01:18 +0900

    add docker

--- src/main/java/com/example/testkube/TestController.java
+++ src/main/java/com/example/testkube/TestController.java
@@ -5,14 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @GetMapping("/health")
+    public boolean health() {
+        return true;
+    }
 
     @GetMapping("/api/version")
     public String version() {
         return "v2";
-    }
-
-    @GetMapping("/actuator/health")
-    public boolean health() {
-        return true;
     }
 }
